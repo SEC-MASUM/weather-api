e3cf5f22c7ccd0eaf062633b5d17810d93c2f235
SEC-MASUM <masum.jess42@gmail.com>
2022-03-03 00:05:45 +0600

    weather app is working perfectly

--- js/app.js
+++ js/app.js
@@ -2,13 +2,34 @@ const API_KEY = "7c4bb91b425af8fdcb75bbfe224e4ad9";
 
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
   fetch(url)
     .then((reponse) => reponse.json())
     .then((data) => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = (data) => {
-  console.log(data);
+  setInnerText("city", data.name || errorMessage(data));
+  setInnerText("temperature", data.main.temp);
+  setInnerText("feels-like", data.main.feels_like);
+  setInnerText("condition", data.weather[0].main);
+  setInnerText(
+    "condition-description",
+    data.weather[0].description.charAt(0).toUpperCase() +
+      data.weather[0].description.slice(1)
+  );
+  // Set weather icon
+  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", iconUrl);
+  // console.log(data);
+};
+
+const errorMessage = (data) => {
+  console.log(data.message.toUpperCase());
 };
